5e34f1e73badb692b65dd188fbd161d057af73d2
georgezouq <zousongqi@foxmail.com>
2023-01-05 14:56:59 +0800

    feat: adjust data detail with search condition

--- packages/mtbird-component-basic/src/components/DataDetail/index.tsx
+++ packages/mtbird-component-basic/src/components/DataDetail/index.tsx
@@ -4,13 +4,13 @@ import manifest from './manifest';
 import styles from './style.module.less';
 
 const DataDetail = ({ node, dataSource, childrenRender, variables, style }: IComponentProps) => {
-  const { targetId, type } = node.data || {};
+  const { targetId, type, search } = node.data || {};
   const [data, setData] = useState<Record<string, any> | undefined>();
 
   const initData = async () => {
     if (!targetId) return;
     if (type === 'model') {
-      const res = await dataSource?.queryDataDetail?.(type, targetId, {});
+      const res = await dataSource?.queryDataDetail?.(type, targetId, search || []);
       setData((res?.data || {}) as Record<string, any>);
     }
   };
