799761a3f92564f9f16f995bdbaeb50ed937a074
georgezouq <zousongqi@foxmail.com>
2023-03-23 16:30:21 +0800

    fix: example bug

--- packages/mtbird-example/src/pages/_app.tsx
+++ packages/mtbird-example/src/pages/_app.tsx
@@ -1,7 +1,22 @@
-import "@/styles/globals.css";
+import React from "react";
+import jsxRuntime from "react/jsx-runtime";
 import "antd/dist/reset.css";
+import "@/styles/globals.css";
+import * as core from "@mtbird/core";
 import type { AppProps } from "next/app";
+import "@mtbird/ui/dist/index.css";
 
+if (process.browser) {
+  (window as any)["react"] = React;
+  (window as any)["jsxRuntime"] = jsxRuntime;
+  (window as any)["react/jsxRuntime"] = jsxRuntime;
+  (window as any)["core"] = core;
+} else {
+  (global as any)["react"] = React;
+  (global as any)["jsxRuntime"] = jsxRuntime;
+  (global as any)["react/jsxRuntime"] = jsxRuntime;
+  (global as any)["core"] = core;
+}
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
