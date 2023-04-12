b34efa4891c9e9d235f8b90e3bcabba2c72223fa
georgezouq <zousongqi@foxmail.com>
2023-04-12 14:23:47 +0800

    feat: try to fix autoplay problem

--- packages/mtbird-component-basic/src/components/Video/index.tsx
+++ packages/mtbird-component-basic/src/components/Video/index.tsx
@@ -1,9 +1,11 @@
 import { IComponentProps } from "@mtbird/shared/src/types/Component";
 import React from "react";
 import manifest from "./manifest";
+import { generateKeys } from "@mtbird/core";
 
 const VideoComponent = ({ node, style }: IComponentProps) => {
   const { props } = node;
+  const id = generateKeys();
   const addProps = {
     ...props,
   };
@@ -13,14 +15,27 @@ const VideoComponent = ({ node, style }: IComponentProps) => {
     addProps["x5-video-orientation"] = "portraint";
   }
 
+  const handleClick = (e: any) => {
+    e.stopPropagation();
+    e.preventDefault();
+    const video = document.getElementById(id) as HTMLVideoElement;
+    if (video.paused) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  };
+
   return (
     <video
       {...props}
+      id={id}
       style={style}
       width={props.style.width}
       height={props.style.height}
       playsinline="true"
       webkit-playsinline="true"
+      onTouchStart={handleClick}
     >
       <source src={props.src as string} type="video/mp4" />
     </video>
